40ce3ce55425313b25a02c4ac2b73d1c1b20f6a8
OliviaDU <604314292@qq.com>
2017-07-05 16:29:02 +0800

    完成评论分页

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,6 +1,8 @@
 /**
  * 每次刷新页面获取一次评论
  */
+let comments = [];
+
 $.ajax({
     type: 'GET',
     url: '/api/comment',
@@ -8,8 +10,22 @@ $.ajax({
         contentid: $('#content-id').val(),
     },
     success: (res) => {
-        renderComment(res.data.comments);
+        comments = res.data.comments;
+        renderComment();
     }
+});
+
+// 评论分页
+let page = 1;
+
+//运用事件委托为a标签绑定事件
+$('.pager').delegate('a', 'click', function () {
+    if ($(this).parent().hasClass('previous')) {
+        --page;
+    } else {
+        ++page;
+    }
+    renderComment();
 });
 
 /**
@@ -18,29 +34,64 @@ $.ajax({
 $('#comment-btn').click(() => {
     $.ajax({
         type: 'POST',
-        url: '/api/comment/post',//发送请求的地址
+
+        //发送请求的地址
+        url: '/api/comment/post',
+
+        //提交的数据
         data: {
             contentid: $('#content-id').val(),
             content: $('#comment-textarea').val()
-        },//提交的数据
+        },
+
+        //提交成功后，res为后端返回的数据
         success: (res) => {
             $('#comment-textarea').val('');
-            renderComment(res.data.comments);
-        }//提交成功后，res为后端返回的数据
+            comments = res.data.comments;
+            renderComment();
+        }
     });
 });
 
 /**
  * 渲染评论
  */
-function renderComment(comments) {
+function renderComment() {
+    //评论分页
+    let len = comments.length;//评论总数
+    if (!len) {
+        $('.pager').html('<p>还没有评论</p>');
+    } else {
+        let limit = 3,//每页显示评论数
+            pages = Math.ceil(len / limit),//评论总页数
 
-    let html = '';
+            start = (page - 1) * limit,
+            end = start + limit;
 
-    //按照时间倒序展示评论
-    comments.reverse().forEach((comment) => {
-        // 评论内容模板
-        let commentContent = `
+        $lis = $('.pager li');
+        $lis.eq(1).html(`<strong>${page}/${pages}</strong>`);
+
+        if (page <= 1) {
+            page = 1;
+            $lis.eq(0).html('<span>没有上一页了</span>');
+        } else {
+            $lis.eq(0).html('<a href="#" >上一页</a>');
+        }
+
+        if (page >= pages) {
+            page = pages;
+            $lis.eq(2).html('<span>没有下一页了</span>');
+        } else {
+            $lis.eq(2).html('<a href="#">下一页</a>');
+        }
+
+        //按照时间倒序展示评论
+        let html = '';
+        comments = comments.reverse();
+
+        comments.slice(start, end).forEach((comment) => {
+            // 评论内容模板
+            let commentContent = `
             <div class="comment-title">
                 <hr>
                 <strong class="pull-left">${comment.username}</strong>
@@ -48,12 +99,14 @@ function renderComment(comments) {
             </div>
             <p>${comment.content}</p>`;
 
-        html += commentContent;
-    });
+            html += commentContent;
+        });
 
-    $('.comment-content').html(html);
+        $('.comment-content').html(html);
+    }
 
-    $('#comment-count').html(`共 ${comments.length} 条评论`);
+  $('#comment-count').html(`共 ${len} 条评论`);
+
 }
 
 /**
@@ -61,5 +114,5 @@ function renderComment(comments) {
  */
 function formData(time) {
     let date = new Date(time);
-    return `${date.getFullYear()}年${date.getMonth()+1}月${date.getDate()}日 ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日 ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
